8c1a6cbaff38789cd5da174216378f1a29459a8d
rahemankazi20-svg <rahemankazi20@gmail.com>
2026-01-26 22:06:19 +0530

    5th commit

--- config/app.php
+++ config/app.php
@@ -80,7 +80,23 @@ return [
 
     'locale' => env('APP_LOCALE', 'en'),
 
- =
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Encryption Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is utilized by Laravel's encryption services and should be set
+    | to a random, 32 character string to ensure that all encrypted values
+    | are secure. You should do this prior to deploying the application.
+    |
+    */
+
+    'cipher' => 'AES-256-CBC',
+
     'key' => env('APP_KEY'),
 
     'previous_keys' => [
